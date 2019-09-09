00ecb7b52de2dac3ef48e72174696e70023eb462
Julia Meyer <juliameyer@outlook.fr>
2019-09-09 16:40:55 +0200

    add style to flatlist and title

--- App.tsx
+++ App.tsx
@@ -23,15 +23,16 @@ export default class App extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-      <Text>Hello Bam !</Text>
-      <View style={styles.movies}>
+        <View style={styles.title_container}>
+          <Text style={styles.title}>The best movies</Text>
+        </View>
+      
       <FlatList
+      style={styles.movies}
           data={this.state.movies}
-          renderItem={({item}: any) => <Text>i{item.title}</Text>}
+          renderItem={({item}: any) => <Text style={styles.item}>{item.title}</Text>}
           keyExtractor={({id}, index) => id}
         />
-      </View>
-      
     </View>
     );
   }
@@ -40,12 +41,27 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
   movies: {
-    backgroundColor: 'red',
-    height: '20%'
+    
+  },
+  title_container: {
+    backgroundColor: '#332E33',
+    height: 100,
+    justifyContent: "flex-end",
+    paddingBottom: 20,
+    padding: 10,
+  },
+  title: {
+    fontSize: 25,
+    color: "white"    
+
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   }
 });
