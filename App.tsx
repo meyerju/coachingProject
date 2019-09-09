e88e13e3c4a7bf2e6f006fe7041f2ce62dd9910f
Julia Meyer <juliameyer@outlook.fr>
2019-09-09 15:43:09 +0200

    fetch movies from facebook api

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+export default class App extends React.Component {
+  state= {
+    movies: null
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          movies: responseJson.movies,
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
       <Text>Hello Bam !</Text>
+      <View style={styles.movies}>
+      <FlatList
+          data={this.state.movies}
+          renderItem={({item}: any) => <Text>i{item.title}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
+      
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +44,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  movies: {
+    backgroundColor: 'red',
+    height: '20%'
+  }
 });
