6113320ef65f395299e0000ab6978ffd5db121da
Julia Meyer <juliameyer@outlook.fr>
2019-09-09 14:36:09 +0200

    Hello Bam

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Bam!</Text>
+      <Text>Hello Bam !</Text>
     </View>
   );
 }
